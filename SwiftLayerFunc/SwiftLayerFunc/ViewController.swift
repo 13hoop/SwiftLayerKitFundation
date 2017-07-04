b3c189f91e082826f4db856cc14f335efbd93add
YongRen <zhanxin13@gmail.com>
2017-07-04 14:35:27 +0800

    using layer than UIImageView display a img

--- SwiftLayerFunc/SwiftLayerFunc/ViewController.swift
+++ SwiftLayerFunc/SwiftLayerFunc/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    view.backgroundColor = UIColor.gray
+    
+    let centerView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+    centerView.center = view.center
+    centerView.backgroundColor = .white
+    view.addSubview(centerView)
+    
+    
+    let myLayer:CALayer = CALayer()
+    myLayer.frame = CGRect(x: 0, y: 0, width: 250, height: 250)
+    myLayer.backgroundColor = UIColor.red.cgColor
+    centerView.layer.addSublayer(myLayer)
+    
+    
+    /// using layer than UIImageView display a img
+    let image = UIImage(named: "FTK.jpg")!
+    myLayer.contents = image.cgImage
+    
   }
 
   override func didReceiveMemoryWarning() {
